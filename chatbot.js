d4b64132ce1452dd20ac62135d3870d007a2835f
HAIDND <dangngochai280603@gmail.com>
2023-10-20 20:11:34 +0700

    test

---
+++ chatbot.js
@@ -0,0 +1,3 @@
+console.log( 123);
+document.write("hehehe do ngoo ngok")
+document.write("<h1>homnayangi</h1><p>Bây giờ, bạn đã chọn nhiều dòng văn bản chung trong trình duyệt. Bạn có thể thực hiện các thao tác tiếp theo trên dãy nhảy này, chẳng hạn như sao chép, cắt, dán, hoặc thay đổi nội dung của chúng</p>")
